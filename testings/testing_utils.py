c9b863f22b3c4d0a550522c68d0f350159bb9842
Francesco-Maria-Benfenati <francesco.benfenati2@studio.unibo.it>
2022-09-29 17:21:10 +0200

    added utils functions (and relative tests) for reproducing LaCasce (2012) solution

---
+++ testings/testing_utils.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 29 13:36:46 2022
+
+@author: Francesco Maria
+"""
+
+# ======================================================================
+# This file is for testing functions implemented ad utils in "utils.py"
+# ======================================================================
+import numpy as np
+import utils
+
+
+def test_constN2_case():
+    """
+    test utils. baroclModes_constN2() function, for N2(z) = const case.
+    """
+    
+    # Parameters
+    N2_0 = 1
+    H = 1000
+    n_modes = 3
+    # Exact Sol.
+    x = np.linspace(0,1,1001)
+    Phi_1 = np.cos(1 * np.pi *x)
+    Phi_2 = np.cos(2 * np.pi *x)
+    Phi_3 = np.cos(3 * np.pi *x)
+    R_1 = 10*np.pi/1
+    R_2 = 10*np.pi/2
+    R_3 = 10*np.pi/3
+    expected_Phi = np.stack((Phi_1, Phi_2, Phi_3), axis=1)
+    expected_R = np.array([R_1, R_2, R_3])
+    # UTILS sol.
+    utils_Phi, utils_R = utils.baroclModes_constN2(N2_0, H, n_modes)
+    # Comparison.
+    Phi_coherence = np.allclose(utils_Phi, expected_Phi, atol = 1e-1)
+    R_coherence = np.allclose(utils_R, expected_R, atol = 0.5)
+    
+    assert np.logical_and(Phi_coherence, R_coherence)
+
+
+def test_expN2_alphaNull():
+    """
+    test utils.baroclModes_expN2() function leads back to Const. case
+    when alpha = 1.
+    """
+    
+    # Parameters
+    gamma = [0.6, 7, 8, 200]
+    alpha = 0
+    H = 1000
+    # Expected Sol.
+    expected_Phi = np.ones([H+1,4])
+    # UTILS sol.
+    utils_Phi = utils.baroclModes_expN2(gamma, alpha, H)
+    
+    assert np.allclose(utils_Phi, expected_Phi, atol = 1e-1)
---
+++ testings/utils.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 29 13:36:30 2022
+
+@author: Francesco Maria
+"""
+
+# ======================================================================
+# This file contains UTILS for testings.
+# ======================================================================
+import numpy as np
+from scipy import special 
+
+
+def baroclModes_constN2(N2_0, H, n_modes):
+    """
+    Computes vertical structure func. (and barocl. rad) for N^2(z)=const.
+
+    Arguments
+    ---------
+    N2_0 : int or float
+        BV frequency squared, constant value 
+    H : int
+        mean depth of the region considered
+    n_modes : int
+        number of modes of motion considered
+
+    Returns
+    -------
+    theor_Phi : numpy.ndarray
+        Theoretical Vertical Structure Function for constant N^2(z)
+    theor_R : numpy.ndarray
+        Theoretical baroclinic deformation radius for const. N^2(z)
+    """
+    
+    integers = np.arange(1, n_modes+1)# "n" integers
+    new_z = np.linspace(0, H, H+1)# new equally spaced depth grid
+    # Compute problem parameter S
+    f_0 = 1e-04
+    L = 1e5
+    S = (N2_0 * H**2)/(f_0**2 * L**2)
+    # Eigenvalues
+    eigenvals = (integers**2)*(np.pi**2)/S # See Pedlosky, GFD book.
+    # Theoretical solution.
+    theor_Phi = np.empty([H+1, n_modes])
+    theor_R = 1/np.sqrt(eigenvals)
+    for i in range(n_modes):
+        theor_Phi[:,i] = np.cos(integers[i] * np.pi * new_z/H)
+  
+    return theor_Phi, theor_R
+
+
+def baroclModes_expN2(gamma, alpha, H):
+    """
+    Computes vertical structure function for exponential N^2(z).
+
+    Arguments
+    ---------
+    gamma : list or numpy.ndarray
+        gamma values
+    alpha : int or float
+        alpha coefficient
+    H : int
+        mean depth of the region considered
+
+    Returns
+    -------
+    theor_Phi : numpy.ndarray
+        Theoretical Vertical Structure Function for exponential N^2(z)
+    
+    # ---------------------------- NOTE --------------------------------
+        N2 is exponential of type N2 = N0 * exp(alpha*z) ; z < 0  
+        
+        alpha = c/H, where c is a real or integer constant
+        
+        gamma_n = (N_0 * lambda_n) /(alpha * f_0), where lambda_n is 
+        the eigenvalue corresponding to mode of motion "n".
+        
+        (see LaCasce, 2012). 
+    # ------------------------------------------------------------------
+    """
+    
+    A = 1 # amplitude
+    n_modes = len(gamma) # n° modes of motion
+    new_z = np.linspace(0, H, H+1)# new equally spaced depth grid
+    # Theoretical Vertical Structure Function Phi(z)
+    theor_Phi = np.empty([H+1, n_modes])
+    for i in range(n_modes):
+        theor_Phi[:,i] = A * (np.exp(alpha*new_z/2)
+                         * (special.yn(0 , 2*gamma[i])
+                         *  special.jv(1, 2*gamma[i]*np.exp(alpha*new_z/2)) 
+                         -  special.jv(0, 2*gamma[i]) 
+                         *  special.yn(1, 2*gamma[i]*np.exp(alpha*new_z/2))
+                            ) )
+        theor_Phi[:,i] /= max(theor_Phi[:,i]) # Norm. theor sol.
+    
+    return theor_Phi
