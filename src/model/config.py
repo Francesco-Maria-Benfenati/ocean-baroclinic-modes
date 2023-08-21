f83075b2b976d121d68fe66188eeb73be641c613
Francesco-Maria-Benfenati <francesco.benfenati5@unibo.it>
2023-08-21 18:11:54 +0200

    updated config file handling

--- src/model/config.py
+++ src/model/config.py
@@ -23,6 +23,7 @@ class Config:
         if not os.path.exists(config_path):
             raise FileNotFoundError(f"Config file {config_path} not found")
         self.__read_config_file__(config_path)
+        self.__from_dict_to_attrs__()
 
     def load_config_file(self, config_path: str) -> None:
         """
@@ -42,20 +43,63 @@ class Config:
         with open(config_path, mode="r") as f:
             self.config_dict = tomllib.loads(f.read())
 
+    def __from_dict_to_attrs__(self) -> None:
+        """
+        Set attributes out of config dictionary.
+        """
+        for k, v in self.config_dict.items():
+            attr = getattr(self, k, v)
+            setattr(self, k, AttrDict(attr))
+
+
+class AttrDict(dict):
+    """
+    Dictionary subclass whose entries can be accessed by attributes
+    """
+
+    def __init__(self, *args, **kwargs) -> dict:
+        def from_nested_dict(data):
+            """Construct nested AttrDicts from nested dictionaries."""
+            if not isinstance(data, dict):
+                return data
+            else:
+                return AttrDict({key: from_nested_dict(data[key]) for key in data})
+
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+        for key in self.keys():
+            self[key] = from_nested_dict(self[key])
+
 
 if __name__ == "__main__":
-    import sys
-    import pandas as pd
+    import numpy as np
     import sys
 
-    config = Config(sys.argv[1])
+    file = "./config.toml"
+    config = Config(file)
+
     print(config.config_file)
     print(config.config_dict)
 
     from pprint import pprint
 
-    key = ""
+    key = "experiment"
     value = config.config_dict[key]
 
     print(value)
     pprint(value)
+    print("Domain mean latitude:")
+    # Access to config values as dictionary items
+    print(np.mean(np.array(config.config_dict["experiment"]["domain"]["lat"])))
+    # Access to config values as attributes
+    print(np.mean(np.array(config.experiment.domain.lat)))
+    print(config.experiment)
+    print("Exp name: ", config.experiment.name)
+    config.experiment.clear()
+    try:
+        print(np.mean(np.array(config.experiment.domain.lat)))
+    except AttributeError:
+        print("experiment values cleared")
+    print(config.input.oce.vars)
+    print(config.input.oce.dims)
