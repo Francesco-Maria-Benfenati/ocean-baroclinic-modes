d5335c809c08d2a116eda4ed169ce9a8c07b5318
Francesco-Maria-Benfenati <francesco.benfenati5@unibo.it>
2023-11-22 18:17:02 +0100

    added folder for examples: how to use the package

---
+++ examples/example_regionmean.py
@@ -0,0 +1,24 @@
+"""
+Example: computing baroclinic modes of motion for a region.
+"""
+
+"""
+USAGE:
+
+from src.folder.name import Name
+
+
+name = Name("project_name")
+
+name.setregion ( ... )
+name.set_ocefile(...)
+
+name.set_mean_region( ... )
+
+
+name.apply_baroclmodes()
+
+
+
+name.plot()
+"""
